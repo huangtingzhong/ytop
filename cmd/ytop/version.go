45e18737ecdad5b3211cc9331258d20340ee1f7d
wwwhtzpw <0281000@gmail.com>
2026-07-03 15:30:24 +0800

    Auto sync: 2026-07-03 15:30:24 - 8 files changed

--- cmd/ytop/version.go
+++ cmd/ytop/version.go
@@ -2,9 +2,9 @@ package main
 
 // Version information
 // This file is automatically updated by build.sh
-// Last updated: 2026-07-03 10:57:16 CST
+// Last updated: 2026-07-03 15:29:51 CST
 var (
-	Version   = "20260703_105716"
-	BuildTime = "2026-07-03 10:57:16 CST"
-	GitCommit = "5bc4f47"
+	Version   = "20260703_152951"
+	BuildTime = "2026-07-03 15:29:51 CST"
+	GitCommit = "2f6b9e6"
 )
